1ac34e21470960f8f925649f232f204f59b54efe
Marius <marius.ionita.portfolio@outlook.com>
2023-10-28 19:43:08 +0300

    Add project files.

---
+++ Codility-CountTriangles/Solution.cs
@@ -0,0 +1,39 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Codility_CountTriangles
+{
+    public class Solution
+    {
+        public const int RANGE_LOWEST_VALUE = 0;
+        public const int RANGE_HIGHEST_VALUE = 1000;
+
+        [MethodImpl(MethodImplOptions.AggressiveOptimization)]
+        public int solution(int[] A)
+        {
+            Array.Sort(A); // Sort the input array
+            int count = 0;
+            int N = A.Length;
+
+            if (N >= RANGE_LOWEST_VALUE && N <= RANGE_HIGHEST_VALUE) 
+            {
+                for (int i = 0; i < N - 2; i++)
+                {
+                    int k = i + 2;
+                    for (int j = i + 1; j < N - 1; j++)
+                    {
+                        // Use binary search to find the index k such that A[i] + A[j] > A[k]
+                        while (k < N && A[i] + A[j] > A[k])
+                            k++;
+                        // The number of triplets that can be formed with A[i] and A[j] is k - j - 1
+                        count += k - j - 1;
+                    }
+                }
+            }
+
+            if(count<0)
+                count = 0;
+
+            return count;
+        }
+    }
+}
---
+++ Codility-CountTrianglesTests/CountTriangles.cs
@@ -0,0 +1,67 @@
+using Codility_CountTriangles;
+
+namespace Codility_CountTrianglesTests
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void Test_Solution_Example()
+        {
+            Solution solution = new Solution();
+            int[] A = { 10, 2, 5, 1, 8, 12 };
+            int expected = 4;
+            int result = solution.solution(A);
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Test_Solution_NoTriplets()
+        {
+            Solution solution = new Solution();
+            int[] A = { 1, 2, 3, 4, 5, 6 };
+            int expected = 7;
+            int result = solution.solution(A);
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Test_Solution_AllTriplets()
+        {
+            Solution solution = new Solution();
+            int[] A = { 3, 3, 3, 3, 3, 3 };
+            int expected = 20; // All combinations of 3 elements are triangular
+            int result = solution.solution(A);
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Test_Solution_EmptyArray()
+        {
+            Solution solution = new Solution();
+            int[] A = { };
+            int expected = 0; // No triplets in an empty array
+            int result = solution.solution(A);
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Test_Solution_LowerBound()
+        {
+            Solution solution = new Solution();
+            int[] A = { 1 };
+            int expected = 0; // Not enough elements to form a triplet
+            int result = solution.solution(A);
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Test_Solution_UpperBound()
+        {
+            Solution solution = new Solution();
+            int[] A = new int[1000]; // All elements are 1
+            int expected = 0; // Number of valid triplets for this input
+            int result = solution.solution(A);
+            Assert.Equal(expected, result);
+        }
+    }
+}
